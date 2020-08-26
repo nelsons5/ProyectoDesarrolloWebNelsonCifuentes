a9c34239cd58eb0e94eb49eb6511ca27583c2256
unknown <ncifuentesc@miumg.edu.gt>
2020-08-26 00:28:07 -0600

    Se mejoro la pagina principal

---
+++ electro-master/js/mijavascript.js
@@ -0,0 +1,16 @@
+function bigImg(x) {
+    x.style.height = "265px";
+    x.style.width = "255px";
+    x.style.border="dotted 5px red"
+    /*
+    x.style.borderTop= "5px inset black";
+    x.style.borderBottom = "5px outset red"; */
+  }
+  
+  function normalImg(x) {
+    x.style.height = "250px";
+    x.style.width = "240px";
+    x.style.border=""
+    x.style.borderTop= "5px inset red";
+    x.style.borderBottom = "5px outset black";
+  }
