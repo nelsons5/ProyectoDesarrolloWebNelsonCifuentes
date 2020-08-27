8f5602b151262c4f5ca6dce7a61173a0d13ff822
unknown <ncifuentesc@miumg.edu.gt>
2020-08-27 02:33:38 -0600

    Se creo la pagina de contacto

--- electro-master/js/mijavascript.js
+++ electro-master/js/mijavascript.js
@@ -15,8 +15,47 @@ function bigImg(x) {
     x.style.borderBottom = "5px outset black";
   }
 
-
+/*WIDGET TABS*/
   $(document).ready(function() {
     $("#proyectTabs").tabs();
-
    });
+
+  /*Validar los campos del formulario*/
+   function validate(){
+     //Obtenemos los valores de los input
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("razon").value;
+    //Error_message es el que se visualizara cuando exista un error
+    var error_message = document.getElementById("error_message");
+    
+    error_message.style.padding = "10px";
+    // creamos una variable llamada text en donde escribiremos el tipo de error
+    var text;
+    if(name.length < 5){
+      text = "Please Enter valid Name";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(!validarEmail(email) || email.length < 6){
+      text = "Ingresa una dirección de correo valida";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(message.length < 30){
+      text = "Escribe por lo menos 30 caracteres";
+      error_message.innerHTML = text;
+      return false;
+    }
+    alert("DATOS ALMACENADOS CORRECTAMENTE");
+    return true;
+  }
+
+  function validarEmail(valor) {
+    //Esta formula nos indicara si el email es correcto debe ser de esta forma: nombre usuario + @ + servidor + dominio
+    //si es corecto devuelve true sino false
+    if ( /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(valor))
+      return true;
+      else 
+      return false;
+    }
